3bb6fa0f1c1da2af0d3be0114e505cba45ad63ca
tedBilgar <mcquicklyss@gmail.com>
2018-07-26 22:15:46 +0500

    Init

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,12 +3,5 @@ package com.company;
 public class Person {
     public void Say(){
         System.out.println("Init");
-        System.out.println("Master");
-        System.out.println("Master2");
-        System.out.println("Master3");
-        System.out.println("Master3");
-        System.out.println("Branch3");
-        System.out.println("Branch3");
-        System.out.println("Branch3");
     }
 }
