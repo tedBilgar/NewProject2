d6f68eab90cb2c079d1cb5c197d4ba09b292ffdc
tedBilgar <mcquicklyss@gmail.com>
2018-07-26 21:54:13 +0500

    Branch3

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,7 +3,6 @@ package com.company;
 public class Person {
     public void Say(){
         System.out.println("Init");
-        System.out.println("Master");
-        System.out.println("Master2");
+        System.out.println("Branch3");
     }
 }
