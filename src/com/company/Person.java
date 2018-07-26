cdff77ced4b89b046948e760090be041cd1eb7d1
tedBilgar <mcquicklyss@gmail.com>
2018-07-26 21:36:55 +0500

    1.1

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Person {
     public void Say(){
-        System.out.println("Branch3AgainSo");
-        System.out.println("еще одна строка мастер");
+
+
     }
 }
